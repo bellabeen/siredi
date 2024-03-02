0ce5275215a29106b5c4d2d9ae53a24c372ed4cb
bellabeen <rootsquid@gmail.com>
2024-03-02 12:59:17 +0700

    [MOV] old framework

--- include/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "siredi";
-
-$connect = mysqli_connect($host, $user, $pass) or die("Koneksi ke database Gagal!");
-mysqli_select_db($connect, $name) or die("Tidak ada database yang dipilih");
-?>
--- include/footer.php
+++
@@ -1,3 +0,0 @@
-<strong>
-<center>Copyright &copy; <?php echo date("Y") ?> KedaiIT<i class="fa fa-heart heart"></i></center>
-</strong>
