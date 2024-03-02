1419e5303f7ad3137f90a4b045d64e52839adbff
bellabeen <rootsquid@gmail.com>
2024-03-02 12:50:47 +0700

    [ADD] gitignore

--- include/connect.php
+++ include/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "bellabeen";
-$pass = "kepoajalu";
-$name = "rekammedis";
+$user = "root";
+$pass = "";
+$name = "siredi";
 
 $connect = mysqli_connect($host, $user, $pass) or die("Koneksi ke database Gagal!");
 mysqli_select_db($connect, $name) or die("Tidak ada database yang dipilih");
